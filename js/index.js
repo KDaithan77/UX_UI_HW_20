86cdc76636a5c14d290a70d476bed7011df798b7
KDaithan77 <52726751+KDaithan77@users.noreply.github.com>
2022-03-08 23:58:51 -0500

    HTML CSS done

--- js/index.js
+++
@@ -1,6 +0,0 @@
-const toggleMenu = document.querySelector(".toggle-menu");
-const navTop = document.querySelector(".nav-top");
- 
-toggleMenu.addEventListener("click", () => {
-  navTop.classList.toggle("is-active");
-});
